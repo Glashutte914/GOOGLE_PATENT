fab7bdba8503a8fc7c9e5751e2a8f1ceda25fb4a
ket850914 <ket850914@gmail.com>
2019-07-21 18:46:26 +0800

    Revert "第三次測試"
    This reverts commit 4b5a6f1fc79bdf02924df4cca950c24aff5a9063.

--- ptt爬蟲.py
+++ ptt爬蟲.py
@@ -9,7 +9,7 @@ import requests
 
 def fetch(url):
     response = requests.get(url)
-    response = requests.get(url, cookies={'over18': '1'})  # 一直向 server 回答滿 18 歲了哈哈 !
+    response = requests.get(url, cookies={'over18': '1'})  # 一直向 server 回答滿 18 歲了 !
     return response
 
 'print(resp.text) # result of setp-1'
